0a47ab72a65397ce344396c51cfc5fc245d89b7e
Amogh Pathak <amogh9792@gmail.com>
2024-06-06 16:42:48 +0530

    data ingestion configurations done..

---
+++ sensor/constant/database.py
@@ -0,0 +1,3 @@
+DATABASE_NAME = "sensor-fault"
+COLLECTION_NAME = "sensor"
+
--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -1,12 +1,11 @@
 from datetime import datetime
 import os
-import sys
-from sensor.exception import SensorException
 from sensor.constant import training_pipeline
 
 
 class TrainingPipelineConfig:
     def __init__(self, timestamp:datetime.now()):
+
         timestamp = timestamp.strftime("%m_%d_%Y_%H_%S")
 
         self.pipeline_name = training_pipeline.PIPELINE_NAME
@@ -19,6 +18,6 @@ class DataIngestionConfig:
         self.data_ingestion_dir: str = os.path.join(training_pipeline_config.artifact_dir, training_pipeline.DATA_INGESTION_DIR_NAME)
         self.feature_store_file_path: str = os.path.join(self.data_ingestion_dir, training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR, training_pipeline.FILE_NAME)
         self.training_file_path: str = os.path.join(self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR, training_pipeline.TRAIN_FILE_NAME)
-        self.test_file_path: str = os.path.join(self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR, training_pipeline.TEST_FILE_NAME)
+        self.testing_file_path: str = os.path.join(self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR, training_pipeline.TEST_FILE_NAME)
         self.train_test_split_ratio: float = training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
         self.collection_name: str = training_pipeline.DATA_INGESTION_COLLECTION_NAME
