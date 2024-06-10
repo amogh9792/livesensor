4751b9cfd66a7c7617924e7b8fc18d9d124056d7
Amogh Pathak <amogh9792@gmail.com>
2024-06-10 19:29:04 +0530

    Data Trasnformation Complete

--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -14,6 +14,7 @@ class DataValidationArtifact:
     invalid_test_file_path: str
     drift_report_file_path: str
 
+@dataclass
 class DataTransformationArtifact:
     transformed_object_file_path: str
     transformed_train_file_path: str
